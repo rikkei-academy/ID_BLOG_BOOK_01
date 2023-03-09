18c38f667139dd36be0e20f6272b7e967b028a79
nguyenminhthoa1002 <minhthoa100299@gmail.com>
2023-03-09 11:30:12 +0700

    feature/#496/register

--- src/main/java/ra/model/repository/UserRepository.java
+++ src/main/java/ra/model/repository/UserRepository.java
@@ -3,11 +3,13 @@ package ra.model.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ra.model.entity.Roles;
 import ra.model.entity.Users;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users,Integer> {
     Users findUsersByUserName(String userName);
     boolean existsByUserName(String userName);
